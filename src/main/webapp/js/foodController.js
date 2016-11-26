8195034385f052d2b0e7b88ce2a3f56e89c291b5
Habiba Shaik <habibashaik@gmail.com>
2016-11-26 15:18:57 -0500

    Angular routing

--- src/main/webapp/js/foodController.js
+++ src/main/webapp/js/foodController.js
@@ -1,13 +1,3 @@
-var app = angular.module('FoodApplication', ['ngResource','ngRoute']);
-console.log("in service js")
-
-//app.factory('UserFactory', function ($resource,$http) {
-//    return $http.get('http://localhost:8080/users/hello')
-//    .success(function(response){
-//        console.log(response);
-//    })
-//});
-
 app.controller('FoodApplicationController', ['$scope', '$http', function ($scope, $http) {
     console.log("in controller")
      $http.get('http://localhost:8080/users/hello')
---
+++ src/main/webapp/js/router.js
@@ -0,0 +1,24 @@
+var app = angular.module('FoodApplication', ['ngResource','ngRoute']);
+
+app.config(['$routeProvider',
+  function ($routeProvider) {
+      $routeProvider.
+          when('/home', {
+              templateUrl: 'pages/login.html'
+          }).
+          when('/search', {
+              templateUrl: 'pages/search.html'
+          }).
+        when('/restlist', {
+            templateUrl: 'pages/restaurantList.html'
+        }).
+        when('/restinfo', {
+            templateUrl: 'pages/restaurantInfo.html'
+        }).
+        when('/checkout', {
+                    templateUrl: 'pages/checkout.html'
+                }).
+        otherwise({
+            redirectTo: '/home'
+        });
+  }]);
